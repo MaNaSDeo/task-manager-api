d4ccbedc2c1c0fb865458e58dd2a472fbc50ecf2
MaNaSDeo <rmnslogin@gmail.com>
2024-03-02 23:46:38 +0530

    Basic express setup

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('Task Manager App')
+const express = require("express");
+const app = express();
+
+//Routes
+app.get("/hello", (request, response) => {
+  response.send("Task Manager App");
+});
+
+const PORT = 3000;
+
+app.listen(PORT, console.log(`server is listening on port ${PORT}...`));
