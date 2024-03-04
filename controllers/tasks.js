89099adb68b11b5f18dafa26d51c5b5ff3fae1ce
MaNaSDeo <rmnslogin@gmail.com>
2024-03-04 10:47:17 +0530

    Basic Schema and Validation

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,11 +1,18 @@
+const Task = require("../models/Tasks");
+
 const getAllTasks = (req, res) => {
   res.send("Get all items");
 };
 const getTask = (req, res) => {
   res.json({ id: req.params.id });
 };
-const createTask = (req, res) => {
-  res.json(req.body);
+const createTask = async (req, res) => {
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).json({ Message: error });
+  }
 };
 const updateTask = (req, res) => {
   res.send("update item");
@@ -21,5 +28,3 @@ module.exports = {
   updateTask,
   deleteTask,
 };
-
-// TayXIU7ZeoQ0OCKO
