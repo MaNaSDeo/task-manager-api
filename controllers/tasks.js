afe2e3a22436e7dcd921bea400f0535a94589247
MaNaSDeo <rmnslogin@gmail.com>
2024-03-05 13:01:25 +0530

    Controllers setup

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,24 +1,63 @@
 const Task = require("../models/Tasks");
 
-const getAllTasks = (req, res) => {
-  res.send("Get all items");
-};
-const getTask = (req, res) => {
-  res.json({ id: req.params.id });
-};
-const createTask = async (req, res) => {
+const getAllTasks = async (req, res) => {
   try {
-    const task = await Task.create(req.body);
-    res.status(201).json({ task });
+    const tasks = await Task.find({});
+    res.status(200).json({ tasks });
   } catch (error) {
     res.status(500).json({ Message: error });
   }
 };
-const updateTask = (req, res) => {
-  res.send("update item");
+
+const getTask = async (req, res) => {
+  try {
+    const { id: taskId } = req.params;
+    const task = await Task.findOne({ _id: taskId });
+    if (!task) {
+      return res.status(404).json({ Message: `No task with id: ${taskId}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ Message: error });
+  }
 };
-const deleteTask = (req, res) => {
-  res.send("delete items");
+
+const createTask = async (req, res) => {
+  try {
+    const task = await Task.create(req.body);
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ Message: error });
+  }
+};
+
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskId } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskId });
+    if (!task) {
+      return res.status(404).json({ Message: `No task with id: ${taskId}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ Message: error });
+  }
+};
+
+const updateTask = async (req, res) => {
+  try {
+    const { id: taskId } = req.params;
+    const task = await Task.findOneAndUpdate({ _id: taskId }, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!task) {
+      return res.status(404).json({ Message: `No task with id: ${taskId}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ Message: error });
+  }
 };
 
 module.exports = {
